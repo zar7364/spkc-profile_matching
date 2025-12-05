98b891119344e37bdd0fe875f23598fe8b6ab01e
Nezar Abdilah Prakasa <68679649+zar7364@users.noreply.github.com>
2025-12-05 11:31:55 +0700

    Refactor app.py to improve code structure

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ st.write("Aplikasi ini menghitung profile matching (gap analysis) dan meranking 
 
 cols = st.columns(100)
 with cols[17]:
-    st.image(r"https://pertanian.uma.ac.id/wp-content/uploads/2019/12/UGM.png", width=1280)
+    st.image("https://pertanian.uma.ac.id/wp-content/uploads/2019/12/UGM.png")
 
 st.write("Oleh:")
 st.subheader("Nezar Abdilah Prakasa (563414)")
@@ -290,6 +290,7 @@ with pd.ExcelWriter(buf2, engine="openpyxl") as writer:
 buf2.seek(0)
 
 st.download_button("Download hasil (xlsx)", buf2, file_name="profile_matching_result.xlsx")
+
 
 
 
